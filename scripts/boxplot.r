eecfd73fb82f1489524818bbf88ad722a9c4e49a
DarioAnongba <dario.anongba@epfl.ch>
2016-05-27 14:03:38 +0200

    Some plots

--- scripts/boxplot.r
+++ scripts/boxplot.r
@@ -1,23 +1,23 @@
-chrN = c('chr1', 'chrX', 'chrY')
-sigmas = c(0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
+chrN = c('chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
+         'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19',
+         'chrX', 'chrY')
+sigmas = c(0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2, 5, 10)
 
 for (chr in chrN) {
-  sizes = rep(0, length(sigmas))
-  
   data <- list()
-  sizes <- rep(0, length(sigmas))
-  
+
   for(i in 1:length(sigmas)) {
-    filename = paste('partitions/', chr, '/partitions_sigma_', sigmas[i], '.Rda', sep = '')
-    partition <- readRDS(file = filename)
+    filename = paste('../partitions/', chr, '/partitions_sigma_', sigmas[i], '.Rda', sep = '')
+    partitions <- readRDS(file = filename)
     
     name = paste('', sigmas[i], '', sep = '')
     
-    data[[name]] <- partition$sizes
-    sizes[i] <- mean(partition$sizes)
+    data[[name]] <- partitions$sizes
   }
   
   #Plot the boxplot
   mainTitle = paste('Boxplot of partition sizes,', chr)
   boxplot(data, main=mainTitle, xlab='sigma values', ylab='Sizes', las=2, col="5")
+  
+  cat('\n')
 }
