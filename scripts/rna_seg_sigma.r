ba7cecc7376317806607e05a32f460511fe87097
DarioAnongba <dario.anongba@epfl.ch>
2016-06-02 02:13:00 +0200

    Alpha and beta fixed bug at storing

--- scripts/rna_seg_sigma.r
+++ scripts/rna_seg_sigma.r
@@ -1,63 +1,62 @@
-source('seg.r')
+source('partitioning.r')
 
 # chrN = c('chr1', 'chrX', 'chrY')
-chrN = c('chr10')
-# sigmas = c(0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2, 5, 10)
-sigmas = c(0)
+chrN = c('chr18')
+percentages = c(59)
 
 for(chr in chrN) {
   #Choice of matrix to use
   rawDataName = paste('../rawData/RNA_seq_', chr, '.txt', sep = '')
   M = read.table(rawDataName, sep="\t", header=T)
-
-  for(sigma in sigmas) {
-    filename = paste('../partitions/', chr, '/partitions_sigma_', sigma, '.Rda', sep = '')
+  
+  for(p in percentages) {
+    filename = paste('../partitions/', chr, '/partitions_percentage_', p, '.Rda', sep = '')
     partitions <- readRDS(file = filename)
     
-    # print(paste("sigma : ", sigma))
-    # print(partitions)
+    #Plotting of the models
+    jj=1
+    K=1
     
-    # #Plotting of the models
-    # jj=1
-    # K=1
-    #
-    # for(k in partitions$sizes)
-    # {
-    #   kk = jj+k-1
-    #   x = M[,jj:kk]
-    #   x = as.matrix(x)
-    #   if(ncol(x)==1) colnames(x) = colnames(M)[jj]
-    #   
-    #   if(partitions$types[K] == 1) mainTitle = paste("Flat partition, sigma =", sigma)
-    #   else mainTitle = paste("Circadian partition, sigma = ", sigma)
-    #   
-    #   # # We only plot the partitions of size < 5
-    #   # if(partitions$sizes[K] < 5) {
-    #   #   print(partitions$Names[K])
-    #   #   
-    #   #   #Plotting Models
-    #   #   matplot(x, pch=1, main=mainTitle)
-    #   #   
-    #   #   m2 = model2(x)
-    #   #   matlines(m2)
-    #   #   
-    #   #   m1 = model1(x)
-    #   #   matlines(m1)
-    #   #   
-    #   #   readline()
-    #   # }
-    #   
-    #   jj=kk+1
-    #   K=K+1
-    # }
-    # 
-    # cat("\n")
-    # cat("\n")
+    print(partitions$sizes)
+    print(partitions$block.types)
+    print(partitions$percentage.resulted)
     
-    s <- partitions$alpha - partitions$beta
-    
-    print(s)
+    for(k in partitions$sizes)
+    {
+      kk = jj+k-1
+      x = M[,jj:kk]
+      x = as.matrix(x)
+      if(ncol(x)==1) colnames(x) = colnames(M)[jj]
+      
+      if(partitions$types[K] == 1) mainTitle = paste("Flat partition, percentage =", p)
+      else mainTitle = paste("Circadian partition, percentage = ", p)
+      
+      # We only plot the partitions of size < 5
+      if(partitions$sizes[K] == 5) {
+        print(partitions$Names[K])
+        
+        print(partitions$alphas[K])
+        print(partitions$betas[K])
+        
+        #Plotting Models
+        matplot(x, pch=1, main=mainTitle)
+        
+        m2 = model2(x)
+        matlines(m2)
+        
+        m1 = model1(x)
+        matlines(m1)
+        
+        readline()
+      }
+      
+      jj=kk+1
+      K=K+1
+    }
     
     readline()
+    
+    cat("\n")
+    cat("\n")
   }
 }
--- scripts/tests.r
+++ scripts/tests.r
@@ -1,5 +1,5 @@
 chrN = c('chr19')
-percentages = seq(5, to = 95, by = 5)
+percentages = c(40)
 
 for (chr in chrN) {
   sizes <- rep(0, length(percentages))
@@ -8,11 +8,6 @@ for (chr in chrN) {
     filename = paste('../partitions/', chr, '/partitions_percentage_', percentages[i], '.Rda', sep = '')
     partitions <- readRDS(file = filename)
     
-    sizes[i] <- mean(partitions$sizes)
+    print(partitions$alpha)
   }
-  
-  #plot the simple mean
-  mainTitle = paste('Average size of partitions,', chr)
-  plot(percentages, sizes, main=mainTitle, xlab='percentage of expected circadian blocks', ylab='Average sizes')
-  lines(percentages, sizes)
 }
